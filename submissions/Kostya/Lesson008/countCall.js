3e309aa497831e1dcf4176406ac94228caa1c97c
Konstantyn Simonov <jastiker@gmail.com>
2020-05-11 17:28:17 +0300

    DZ8 countCall

---
+++ submissions/Kostya/Lesson008/countCall.js
@@ -0,0 +1,13 @@
+
+const func = () =>{
+    let count = 0;
+    return function () {
+        console.log(count);
+        return count++;
+    }
+}
+
+let counter1 = func();
+
+counter1();
+counter1();
